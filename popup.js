4acb4a210bd7704e799bc8c4dbcf4e004b3f4cdd
amazondelivery <48974775+amazondelivery@users.noreply.github.com>
2025-08-03 10:08:55 -0700

    workign on countdown

--- popup.js
+++ popup.js
@@ -2,6 +2,13 @@ let isSettingsVisible = false;const settingsButton = document.getElementById("se
 const settingsPage = document.getElementById("settingsPage");
 const siteList = document.getElementById("siteList");
 const saveButton = document.getElementById("saveSites");
+const actualSettingsPage = document.getElementById("actualSettingsPage");
+const settingsLockMessage = document.getElementById("settingsLock");
+const lockSettingsPicker = document.getElementById("lockSettingsPicker");
+
+let countdown = 0;
+
+chrome.storage.local.set({ countdownSetting: 0 }, () => {});
 
 chrome.storage.local.get("grayscaleSites", (result) => {
     const sites = result.grayscaleSites;
@@ -9,7 +16,7 @@ chrome.storage.local.get("grayscaleSites", (result) => {
     if (sites && Array.isArray(sites)) {
         siteList.value = sites.join('\n');
     } else {
-        
+        console.log("uh oh");
     }
 });
 
@@ -18,14 +25,29 @@ saveButton.addEventListener('click', () => {
         .split('\n')
         .map(line => line.trim())
         .filter(line => line !== '');
-    chrome.storage.local.set({ grayscaleSites: updatedSiteList }, () => {
-        console.log("Grayscale sites saved on update");
-    });
 });
 
 settingsButton.addEventListener('click', () => {
     isSettingsVisible = !isSettingsVisible;
     settingsPage.style.display = isSettingsVisible ? "block" : "none";
+    countdownSetting = chrome.storage.local.get("countdownSetting", (result) => {
+        if (countdownSetting != 0) {
+            countdown = countdownSetting;
+            const interval = setInterval(() => {
+                if (countdown > 0) {
+                    settingsLockMessage.textContent = `Settings is disabled for ${countdown} seconds`;
+                    countdown--;
+                } else {
+                    clearInterval(interval);
+                }
+            }, 1000);
+        }
+    });
+    actualSettingsPage.style.display = isSettingsVisible ? "block" : "none";
+});
+
+lockSettingsPicker.addEventListener('change', function() {
+    chrome.storage.local.set({"countdownSetting":parseInt(this.value, 10)});
 });
 
 document.getElementById("feedback").addEventListener("click", () => {
